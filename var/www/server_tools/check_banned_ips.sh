4a5e6812fa8f8ac3fcf28f923b7b0551993c99be
After Dark <tvasile@gmail.com>
2025-04-12 21:32:35 +0300

    Automated commit: Sat Apr 12 21:32:35 EEST 2025

--- var/www/server_tools/check_banned_ips.sh
+++ var/www/server_tools/check_banned_ips.sh
@@ -1,9 +1,26 @@
-#!/bin/bash
-while read host; do
-    ip=$(host "$host" | awk '/has address/ { print $4 }')
-    if [[ $ip ]]; then
-        echo "$host resolves to $ip"
-    else
-        echo "$host could not be resolved"
+# Unban all Googlebot IPs by resolving hostnames
+for host in \
+  crawl-66-249-79-35.googlebot.com \
+  crawl-66-249-79-34.googlebot.com \
+  crawl-66-249-79-1.googlebot.com \
+  crawl-66-249-77-69.googlebot.com \
+  crawl-66-249-77-160.googlebot.com \
+  crawl-66-249-71-64.googlebot.com \
+  crawl-66-249-66-199.googlebot.com \
+  crawl-66-249-66-198.googlebot.com \
+  crawl-66-249-66-197.googlebot.com \
+  crawl-66-249-65-65.googlebot.com \
+  crawl-66-249-65-38.googlebot.com \
+  crawl-66-249-65-37.googlebot.com \
+  crawl-66-249-64-130.googlebot.com \
+  crawl-66-249-64-129.googlebot.com \
+  crawl-66-249-64-128.googlebot.com \
+  cache.google.com; do
+
+    ip=$(host $host | awk '/has address/ { print $4 }')
+    if [ ! -z "$ip" ]; then
+        echo "Unbanning $host ($ip)"
+        sudo iptables -D f2b-nginx-forbidden -s "$ip" -j REJECT
+        sudo iptables -D f2b-SSH -s "$ip" -j REJECT 2>/dev/null
     fi
-done < "/var/www/server_tools/iptables.txt"
+done
